06b0e9bed740b6858479dcdb05ddc6ba375b5903
unknown <DeMinhDeRozan>
2026-08-28 22:17:16 +1000

    First commit

---
+++ host_s3.py
@@ -0,0 +1,33 @@
+import boto3
+
+s3_client = boto3.client(
+    's3',
+    endpoint_url='http://localhost:4566',
+    aws_access_key_id='test',
+    aws_secret_access_key='test',
+    region_name='ap-southeast-2'
+)
+
+bucket_name = 'my-bucket-1'
+file_key = 'HUM.html'
+
+try:
+    print(f" Loading '{file_key}' from bucket '{bucket_name}'...")
+    response = s3_client.get_object(Bucket=bucket_name, Key=file_key)
+    
+    content = response['Body'].read().decode('utf-8')
+    print("\n---READ CONTENT")
+    print(content[:300] + "..." if len(content) > 300 else content)
+    print("--------------------------------------\n")
+
+    url = s3_client.generate_presigned_url(
+        'get_object',
+        Params={'Bucket': bucket_name, 'Key': file_key},
+        ExpiresIn=3600 #1 hour expiry
+    )
+    
+    print(f"Successfully")
+    print(f"Presigned URL: {url}")
+
+except Exception as e:
+    print(f" File not found {e}")
---
+++ main.tf
@@ -0,0 +1,57 @@
+variable "region" {
+    type = string
+    default = "ap-southeast-2"
+}
+
+variable "bucket-name" {
+    type = string
+    default = "my-bucket-1"
+}
+
+provider "aws" {
+  region = var.region
+  access_key = "test"
+  secret_key = "test"
+  skip_credentials_validation = true
+  skip_metadata_api_check = true
+  skip_requesting_account_id = true
+  s3_use_path_style = true
+  endpoints {
+    s3 = "http://localhost:4566"
+  }
+}
+
+resource "aws_s3_bucket" "bucket1" {
+    bucket = var.bucket-name
+    tags = {
+        Name = var.bucket-name
+    }
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket1" {
+    bucket = aws_s3_bucket.bucket1.id
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
+}
+
+resource "aws_s3_object" "upload_and_update" {
+  bucket = aws_s3_bucket.bucket1.id
+  key = "HUM.html"
+  source = "1/1.html"
+  content_type = "text/html"
+  depends_on = [ aws_s3_bucket.bucket1 ]
+  provisioner "local-exec" {
+    command = "python host_s3.py" #presigned link for s3
+  }
+}
+
+
+resource "aws_s3_bucket_ownership_controls" "bucket1" {
+  bucket = aws_s3_bucket.bucket1.id
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
